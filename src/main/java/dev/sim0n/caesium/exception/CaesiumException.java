98bc36347b7f6770759473f4e0e0f4de6f8e6db0
Martin-Westwood <120451294+Martin-Westwood@users.noreply.github.com>
2024-09-03 20:26:51 +0800

    Update CaesiumException.java
    删减

--- src/main/java/dev/sim0n/caesium/exception/CaesiumException.java
+++ src/main/java/dev/sim0n/caesium/exception/CaesiumException.java
@@ -1,8 +1,2 @@
 package dev.sim0n.caesium.exception;
 
-public class CaesiumException extends Exception {
-
-    public CaesiumException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
